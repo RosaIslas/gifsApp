25724ba74770ba5d339915dbb95e215bef586142
RosaIslas <rosanayeliaguilar@gmail.com>
2023-06-07 22:44:54 -0500

    Gifs app update

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams  } from '@angular/common/http';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
     providedIn: 'root'
 })
 export class GifsService {
 
+    public gifsList: Gif[] = [];
     private _tagsHistory: string[] = [];
     private apiKey: string = 'R38SfJiiFe7Owd8fEnDLyPwtyYLcBAI2';
     private serviceUrl = 'https://api.giphy.com/v1/gifs';
 
     constructor( private http: HttpClient ) {
-
+        this.loadLocalStorage();
     }
 
     get tagsHistory(){
@@ -28,6 +30,18 @@ export class GifsService {
         this._tagsHistory.unshift( tag );
 
         this._tagsHistory = this.tagsHistory.splice(0,10);
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage():void {
+        localStorage.setItem('history', JSON.stringify( this._tagsHistory ));
+    }
+
+    private loadLocalStorage():void{
+        if(!localStorage.getItem('history')) return ;
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+        if(this._tagsHistory.length === 0) return ;
+        this.searchTag(this._tagsHistory[0]);
     }
 
      searchTag ( tag:string ): void {
@@ -40,9 +54,10 @@ export class GifsService {
         .set('limit', '10')
         .set('q', tag);
         
-        this.http.get(`${this.serviceUrl}/search?${ params }`)
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search?${ params }`)
         .subscribe( resp => {
-            console.log(resp);
+            this.gifsList = resp.data;
+            console.log(this.gifsList);
             
         })
         // fetch('https://api.giphy.com/v1/gifs/search?api_key=R38SfJiiFe7Owd8fEnDLyPwtyYLcBAI2&q=valorant&limit=10')
